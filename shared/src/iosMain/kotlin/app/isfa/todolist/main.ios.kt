7520f37d6a2b0618e6f94ee7477eb75cfbf73702
isfaaghyth <isfaaghyth@gmail.com>
2023-05-27 16:46:40 +0700

    feat: setup compose on ContentView in iosApp module

---
+++ shared/src/iosMain/kotlin/app/isfa/todolist/main.ios.kt
@@ -0,0 +1,13 @@
+/**
+ * This suppress need to enable due the ViewController below
+ * only used on ContentView.swift
+ */
+@file:Suppress("unused", "FunctionName")
+
+package app.isfa.todolist
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController {
+
+}
