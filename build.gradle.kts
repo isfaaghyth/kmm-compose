03ad82f80bacb8a8b3a9b0f1a1e297d0f729ee99
isfaaghyth <isfaaghyth@gmail.com>
2023-05-27 16:43:51 +0700

    feat: setup the compose multiplatform dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.1.0-beta02").apply(false)
-    id("com.android.library").version("8.1.0-beta02").apply(false)
-    kotlin("android").version("1.8.10").apply(false)
-    kotlin("multiplatform").version("1.8.10").apply(false)
+    kotlin("multiplatform").apply(false)
+    kotlin("plugin.serialization").apply(false)
+    id("com.android.application").apply(false)
+    id("com.android.library").apply(false)
+    id("org.jetbrains.compose").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,39 @@
+rootProject.name = "TODO List"
+
+include(":androidApp")
+include(":shared")
+
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
-        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+    }
+
+    plugins {
+        kotlin("jvm").version("1.8.10")
+        kotlin("plugin.serialization").version("1.8.10")
+        kotlin("multiplatform").version("1.8.10")
+        kotlin("android").version("1.8.10")
+
+        id("com.android.application").version("7.4.2")
+        id("com.android.library").version("7.4.2")
+
+        id("org.jetbrains.compose").version("1.4.0")
     }
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.catalogs.toml"))
+        }
+    }
+
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
-
-rootProject.name = "TODO_List"
-include(":androidApp")
-include(":shared")
