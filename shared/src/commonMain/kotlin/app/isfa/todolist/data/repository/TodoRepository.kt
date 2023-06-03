e029e0cd12756d2947e0bd116587f5a4d5eac349
isfaaghyth <isfaaghyth@gmail.com>
2023-06-03 18:38:41 +0700

    feat: create todo repository

---
+++ shared/src/commonMain/kotlin/app/isfa/todolist/data/repository/TodoRepository.kt
@@ -0,0 +1,54 @@
+package app.isfa.todolist.data.repository
+
+import app.isfa.todolist.data.TodoState
+import app.isfa.todolist.data.entity.Todo
+import com.benasher44.uuid.Uuid
+import com.benasher44.uuid.uuid4
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.datetime.Clock
+
+interface TodoRepository {
+    val state: StateFlow<TodoState>
+
+    fun add(text: String)
+    fun remove(uuid: Uuid)
+}
+
+class TodoRepositoryImpl : TodoRepository {
+
+    override val state: StateFlow<TodoState>
+        get() = _state
+
+    private val _state = MutableStateFlow(TodoState())
+
+    override fun add(text: String) {
+        _state.value = state.value.copy()
+            .also {
+                it.list.add(
+                    Todo(
+                        uuid = uuid4(),
+                        text = text,
+                        createdAt = Clock
+                            .System
+                            .now()
+                            .epochSeconds
+                    )
+                )
+            }
+    }
+
+    override fun remove(uuid: Uuid) {
+        _state.value = state.value.copy()
+            .also { state ->
+                val list = state.list
+
+                val todo = list.find { it.uuid == uuid } ?: return
+                val index = list.indexOf(todo)
+
+                if (index > -1) {
+                    list.removeAt(index)
+                }
+            }
+    }
+}
