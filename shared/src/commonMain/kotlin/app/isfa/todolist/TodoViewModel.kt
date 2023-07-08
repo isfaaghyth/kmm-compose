d1fe43fe373d086dcb658a04836062d9d48e6ce0
isfaaghyth <isfaaghyth@gmail.com>
2023-07-08 19:52:49 +0700

    feat: (mobius) glue the mobius loop and UI layer

--- shared/src/commonMain/kotlin/app/isfa/todolist/TodoViewModel.kt
+++ shared/src/commonMain/kotlin/app/isfa/todolist/TodoViewModel.kt
@@ -1,54 +1,55 @@
 package app.isfa.todolist
 
-import app.isfa.todolist.data.TodoEvent
-import app.isfa.todolist.data.TodoState
+import app.isfa.todolist.data.model.TodoModel
+import app.isfa.todolist.data.model.TodoState
+import app.isfa.todolist.data.repository.FeatureToggleRepository
 import app.isfa.todolist.data.repository.TodoRepository
+import app.isfa.todolist.effects.TodoEffectHandler
+import app.isfa.todolist.event.TodoEvent
+import app.isfa.todolist.event.TodoUpdate
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
+import kt.mobius.flow.FlowMobius
+import kt.mobius.functions.Consumer
 import kotlin.coroutines.CoroutineContext
 
 class TodoViewModel constructor(
-    private val repository: TodoRepository
+    todoRepository: TodoRepository,
+    featureToggle: FeatureToggleRepository,
 ) : TodoViewModelContract, CoroutineScope {
 
     override val coroutineContext: CoroutineContext
         get() = Job() + Dispatchers.Main
 
-    override val state: StateFlow<TodoState>
-        get() = repository.state
+    override val loop = FlowMobius.loop(
+        TodoUpdate(),
+        TodoEffectHandler(
+            todoRepository,
+            featureToggle
+        ).create()
+    )
 
-    private val _event = MutableSharedFlow<TodoEvent>(replay = 50)
+    override val model: StateFlow<TodoModel>
+        get() = _model
+
+    private val _loop = loop.startFrom(
+        TodoModel(state = TodoState.EMPTY)
+    )
+
+    private val _model = MutableStateFlow(
+        _loop.mostRecentModel
+    )
 
     init {
-        launch {
-            _event
-                .distinctUntilChanged()
-                .collect { event ->
-                    when (event) {
-                        is TodoEvent.Add -> {
-                            repository.onShowAddDialog(true)
-                        }
-                        is TodoEvent.Save -> {
-                            repository.onShowAddDialog(false)
-
-                            if (event.text.isNotEmpty()) {
-                                repository.add(event.text)
-                            }
-                        }
-                        is TodoEvent.Remove -> {
-                            repository.remove(event.uuid)
-                        }
-                    }
-                }
+        _loop.observe { model ->
+            _model.value = model
         }
     }
 
     override fun setAction(action: TodoEvent) {
-        _event.tryEmit(action)
+        _loop.dispatchEvent(action)
     }
 }
