0fb7de39a4f980dd8c77dff39e204affbf9e2af3
isfaaghyth <isfaaghyth@gmail.com>
2023-07-08 19:50:22 +0700

    feat: (mobius) re-initiated repositories for todo and feature toggle

--- shared/src/commonMain/kotlin/app/isfa/todolist/data/repository/TodoRepository.kt
+++ shared/src/commonMain/kotlin/app/isfa/todolist/data/repository/TodoRepository.kt
@@ -1,61 +1,67 @@
 package app.isfa.todolist.data.repository
 
-import app.isfa.todolist.data.TodoState
 import app.isfa.todolist.data.entity.Todo
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuid4
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.datetime.Clock
 
 interface TodoRepository {
-    val state: StateFlow<TodoState>
 
-    fun add(text: String)
-    fun remove(uuid: Uuid)
-    fun onShowAddDialog(show: Boolean)
+    fun getTodoList(): Flow<List<Todo>>
+    fun insertTodo(content: String)
+    fun deleteTodo(uuid: Uuid)
 }
 
 class TodoRepositoryImpl : TodoRepository {
 
-    override val state: StateFlow<TodoState>
-        get() = _state
-
-    private val _state = MutableStateFlow(TodoState())
-
-    override fun add(text: String) {
-        _state.value = state.value.copy()
-            .also {
-                it.list.add(
-                    Todo(
-                        uuid = uuid4(),
-                        text = text,
-                        createdAt = Clock
-                            .System
-                            .now()
-                            .epochSeconds
-                    )
-                )
-            }
-    }
-
-    override fun remove(uuid: Uuid) {
-        _state.value = state.value.copy()
-            .also { state ->
-                val list = state.list
-
-                val todo = list.find { it.uuid == uuid } ?: return
-                val index = list.indexOf(todo)
-
-                if (index > -1) {
-                    list.removeAt(index)
-                }
-            }
-    }
-
-    override fun onShowAddDialog(show: Boolean) {
-        _state.value = state.value.copy(
-            isTodoAdd = show
+    private val data = mutableListOf(
+        Todo(
+            uuid = uuid4(),
+            text = "Foo",
+            createdAt = Clock
+                .System
+                .now()
+                .epochSeconds
+        ),
+        Todo(
+            uuid = uuid4(),
+            text = "Bar",
+            createdAt = Clock
+                .System
+                .now()
+                .epochSeconds
+        ),
+        Todo(
+            uuid = uuid4(),
+            text = "Loren",
+            createdAt = Clock
+                .System
+                .now()
+                .epochSeconds
+        ),
+        Todo(
+            uuid = uuid4(),
+            text = "Ipsum",
+            createdAt = Clock
+                .System
+                .now()
+                .epochSeconds
         )
+    )
+
+    override fun getTodoList(): Flow<List<Todo>> {
+        return flow {
+            emit(data)
+        }
+    }
+
+    override fun insertTodo(content: String) {
+
+    }
+
+    override fun deleteTodo(uuid: Uuid) {
+
     }
 }
