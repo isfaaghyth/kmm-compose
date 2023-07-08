4e54cc98b4cb719905d79d048af1993480d147ef
isfaaghyth <isfaaghyth@gmail.com>
2023-07-08 23:48:43 +0700

    feat: (todo-add) add todo into data sources

--- shared/src/commonMain/kotlin/app/isfa/todolist/data/repository/TodoRepository.kt
+++ shared/src/commonMain/kotlin/app/isfa/todolist/data/repository/TodoRepository.kt
@@ -3,6 +3,7 @@ package app.isfa.todolist.data.repository
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import app.isfa.todolist.data.entity.Todo
+import app.isfa.todolist.util.currentTime
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuid4
 import kotlinx.coroutines.flow.Flow
@@ -31,10 +32,17 @@ class TodoRepositoryImpl : TodoRepository {
     }
 
     override fun insertTodo(content: String) {
-
+        data.add(0, createTodoModel(content))
     }
 
     override fun deleteTodo(uuid: Uuid) {
 
     }
+
+    private fun createTodoModel(content: String) =
+        Todo(
+            uuid4(),
+            content,
+            currentTime()
+        )
 }
